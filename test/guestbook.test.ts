bd902694bb65397a3f3ab2b29d83f2b30d2645db
VP-MaxHax <vp.juselius@gmail.com>
2025-10-24 17:07:47 +0300

    Initial commit

---
+++ test/guestbook.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Guestbook from '../lib/guestbook-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/guestbook-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Guestbook.GuestbookStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
